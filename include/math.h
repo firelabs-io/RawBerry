a1c114f129bb01b407bafe8f08760e6ffe41f9a4
firelabs-io <firelabs-io@gmail.com>
2024-12-22 16:03:33 -0300

    modified:   include/math.h 	modified:   kernel/main.c

--- include/math.h
+++ include/math.h
@@ -30,19 +30,26 @@ int stol(const char* str) {
     return result;
 }
 
-void memory_info(const int32_t start_address, int32_t last_address){
-    int32_t result = last_address - start_address;
-    if (result < 1024) {
-        vga_print_string(itol(result));
+void memory_info(memory_entry* memory_table, int32_t size) {
+    int32_t total_memory = 0;
+
+    // Iterate over all the memory blocks in the table
+    for (int i = 0; i < size; i++) {
+        total_memory += (memory_table[i].end_addr - memory_table[i].start_addr);
+    }
+
+    // Calculate and print the total memory usage
+    if (total_memory < 1024) {
+        vga_print_string(itol(total_memory));
         vga_print_string(" bytes\n");
-    } else if (result < 1024 * 1024) { // Less than 1 MB
-        vga_print_string(itol(result / 1024));
+    } else if (total_memory < 1024 * 1024) { // Less than 1 MB
+        vga_print_string(itol(total_memory / 1024));
         vga_print_string(" KB\n");
-    } else if (result < 1024 * 1024 * 1024) { // Less than 1 GB
-        vga_print_string(itol(result / (1024 * 1024)));
+    } else if (total_memory < 1024 * 1024 * 1024) { // Less than 1 GB
+        vga_print_string(itol(total_memory / (1024 * 1024)));
         vga_print_string(" MB\n");
     } else { // 1 GB or more
-        vga_print_string(itol(result / (1024 * 1024 * 1024)));
+        vga_print_string(itol(total_memory / (1024 * 1024 * 1024)));
         vga_print_string(" GB\n");
     }
 }
--- kernel/main.c
+++ kernel/main.c
@@ -37,15 +37,20 @@ void memory_print(memory_entry* memory_table, int32_t size){
     int i = 0;
     while (i < size){
         vga_print_string("block: ");
-        vga_print_string(itol(memory_table[i].start_addr));
+        vga_print_string(itol(memory_table[i].start_addr));  // Print start address
         vga_print_string(" ");
-        vga_print_string(itol(memory_table[i].end_addr));
+        vga_print_string(itol(memory_table[i].end_addr));    // Print end address
         vga_print_string(" ");
-        vga_print_string(memre(memory_table[i].start_addr, memory_table[i].end_addr));  
+        
+        // Read and print memory contents from start to end address
+        char* memory_contents = memre(memory_table[i].start_addr, memory_table[i].end_addr);
+        vga_print_string(memory_contents);  // Print the contents of the memory block
         vga_print_string("\n");
+        
         i++;
     }
 }
+
 void memory_write(memory_entry* memory_table, int32_t start, int32_t end, int32_t index, const char* string){
     memwr(string, start, end);
     memory_table[index].start_addr = start;
@@ -56,7 +61,7 @@ void launch_kernel(void) {
     vga_initialize();
     vga_print_string("Welcome to RawBerry OS!\n");
     vga_print_string("[YANE TERMINAL 1.0.0]\n");
-    memory_entry memory_table[12] = {
+    memory_entry memory_table[15] = {
         {0x400000,0x400000+Strlen("Hello, RawBerryOS!")}
     };
 
@@ -96,14 +101,14 @@ void launch_kernel(void) {
             vga_print_string("Written to memory.\n");
 
         } else if (strEql(strSlice(Keyboard_buffer, 0, 4), "read") == 0) {
-            memory_print(memory_table, 11);
+            memory_print(memory_table, 15);
             char* buffer = memre(start_address, last_address);
             //vga_print_string("Read: ");
             //vga_print_string(buffer);
         } else if (strEql(strSlice(Keyboard_buffer, 0, 5), "crash") == 0){
             asm volatile ("ud2");
         } else if (strEql(strSlice(Keyboard_buffer, 0, 9), "mem-usage") == 0){
-            memory_info(start_address, last_address);
+            memory_info(memory_table, 15);
         }
     }
     return;
